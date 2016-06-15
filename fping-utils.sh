55a6a01162a3e5d2970b09080ee2f763e0a74101
bjellesma <william.jellesma@gmail.com>
2016-06-14 22:01:02 -0400

    Script is working

--- fping-utils.sh
+++ fping-utils.sh
@@ -1,6 +1,9 @@
+mkdir -p fping-utils
+cd fping-utils
 echo "Please enter the first IP in your range (example: 127.0.0.1)"
 read startIP
 echo "Please enter the last IP in your range (example: 127.0.0.254)"
 read endIP
-$filename='fping' . $(date)
+$filename="fping$(date).txt"
 fping -a -g $startIP $endIP > fping-utils/$filename
+echo "Your ping sweep has finished. Please check your fping-utils directory"
